0bc069f0089f56b1e7087629923a4fba645b0991
Jack Firth <jackhfirth@gmail.com>
2015-07-06 12:52:04 -0700

    Add documentation coverage test, remove undocumented exports

--- lenses/main.rkt
+++ lenses/main.rkt
@@ -1,13 +1,25 @@
 #lang racket
 
-(require "core/main.rkt"
-         "list/main.rkt"
-         "syntax.rkt"
-         "syntax-keyword.rkt")
-
-(provide
- (all-from-out
+(require
   "core/main.rkt"
   "list/main.rkt"
   "syntax.rkt"
-  "syntax-keyword.rkt"))
+  "syntax-keyword.rkt")
+
+(provide
+ (except-out
+  (all-from-out
+   "core/main.rkt"
+   "list/main.rkt"
+   "syntax.rkt"
+   "syntax-keyword.rkt")
+  apply-lens
+  drop-lens
+  lens-set*
+  lens-thrush
+  lens-transform*
+  lens-view*
+  list-ref-lens
+  list-ref-nested-lens
+  take-lens
+  use-applicable-lenses!))
