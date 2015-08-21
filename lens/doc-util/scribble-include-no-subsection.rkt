e4a73321d830c26ad0734e38417f4b28923fb49c
AlexKnauth <alexander@knauth.org>
2015-08-21 14:09:37 -0400

    put a few parens back

--- lens/doc-util/scribble-include-no-subsection.rkt
+++ lens/doc-util/scribble-include-no-subsection.rkt
@@ -15,20 +15,16 @@ require syntax/parse/define
 ;;  - has exactly one (list 'part (generated-tag)) tag
 ;;  - has no subsections
 ;;  - has no `to-collect` content
-define-syntax scribble-include/no-subsection
-  syntax-parser
-    (~and stx (scribble-include/no-subsection mod))
-     #:with doc-from-mod
-     datum->syntax #'mod 'doc
-     unless (module-path? (syntax->datum #'mod))
-       raise-syntax-error #f
-                          "not a module path"
-                          #'stx
-                          #'mod
-     syntax
-       begin
-         require (only-in mod [doc-from-mod doc])
-         match-define
-           part #f (list (list 'part (generated-tag))) #f style '() blocks '()
-           doc
-         nested-flow style blocks
+(define-syntax scribble-include/no-subsection
+  (syntax-parser
+    [(~and stx (scribble-include/no-subsection mod))
+     #:with doc-from-mod (datum->syntax #'mod 'doc)
+     (unless (module-path? (syntax->datum #'mod))
+       (raise-syntax-error #f
+                           "not a module path"
+                           #'stx
+                           #'mod))
+     #'(begin
+         (require (only-in mod [doc-from-mod doc]))
+         (match-define (part #f (list (list 'part (generated-tag))) #f style '() blocks '()) doc)
+         (nested-flow style blocks))]))
