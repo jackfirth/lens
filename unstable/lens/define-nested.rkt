67b5ae542111421707c922b166c2334747c824ab
AlexKnauth <alexander@knauth.org>
2015-12-04 23:57:02 -0500

    allow define-nested-lenses clauses to contain other clauses

--- unstable/lens/define-nested.rkt
+++ unstable/lens/define-nested.rkt
@@ -23,18 +23,29 @@ begin-for-syntax
   ;; helper syntax-class for define-nested-lenses
   (define-syntax-class (clause base-id base-lens-tmp)
     #:attributes (def)
-    [pattern [suffix-id:id suffix-lens-expr:expr]
+    [pattern [suffix-id:id suffix-lens-expr:expr
+               unchecked-clause ...]
              #:with base-lens:id base-lens-tmp
-             #:do [(define-values [base-suffix-lens-id sub-range-binders]
+             #:do [(define-values [base-suffix-id base-suffix-sub-range]
                      (id-append #:context base-id
-                                base-id -- #'suffix-id -lens))]
+                                base-id -- #'suffix-id))
+                   (define-values [base-suffix-lens-id base-suffix-lens-sub-range]
+                     (id-append #:context base-id
+                                base-suffix-id -lens))]
+             #:with base-suffix
+             base-suffix-id
              #:with base-suffix-lens
              base-suffix-lens-id
+             #:with [(~var clause (clause #'base-suffix #'base-suffix-lens)) ...]
+             #'[unchecked-clause ...]
              #:with def
              (with-sub-range-binders
-              #`(define base-suffix-lens
-                  (lens-thrush base-lens suffix-lens-expr))
-              sub-range-binders)])
+              #'(begin
+                  (define base-suffix-lens
+                    (lens-thrush base-lens suffix-lens-expr))
+                  clause.def
+                  ...)
+              base-suffix-lens-sub-range)])
 
 
 (define-syntax define-nested-lenses
@@ -52,8 +63,12 @@ module+ test
   (define-nested-lenses [first first-lens]
     [first first-lens]
     [second second-lens]
-    [third third-lens])
+    [third third-lens
+      [first first-lens]
+      [second second-lens]])
   (check-equal? (lens-view first-first-lens '((a b c d) e)) 'a)
   (check-equal? (lens-view first-second-lens '((a b c d) e)) 'b)
   (check-equal? (lens-view first-third-lens '((a b c d) e)) 'c)
+  (check-equal? (lens-view first-third-first-lens '((a b (c d) e) f)) 'c)
+  (check-equal? (lens-view first-third-second-lens '((a b (c d) e) f)) 'd)
 
