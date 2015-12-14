7ab893af5ed30f01ba6ed7b3615f6bbffb9cf784
AlexKnauth <alexander@knauth.org>
2015-12-13 22:55:39 -0500

    move more unstable code to lens/private

---
+++ lens/private/list/sublist.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(provide sublist-lens)
+
+(require lens
+         lens/private/list/list-ref-take-drop)
+
+(module+ test
+  (require rackunit))
+
+(define (sublist-lens i j)
+  (lens-thrush (take-lens j) (drop-lens i)))
+
+(module+ test
+  (check-equal? (lens-view (sublist-lens 1 4) '(0 1 2 3 4 5))
+                '(1 2 3))
+  (check-equal? (lens-set (sublist-lens 1 4) '(0 1 2 3 4 5) '(a b c))
+                '(0 a b c 4 5))
+  )
