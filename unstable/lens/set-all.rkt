4ae4d31ff5acfc0b377484c93cc826cf2dcc8d6b
AlexKnauth <alexander@knauth.org>
2016-01-05 19:54:19 -0500

    move reverse.rkt and set-all.rkt to lens/private

--- unstable/lens/set-all.rkt
+++ unstable/lens/set-all.rkt
@@ -1,20 +1,2 @@
-#lang racket/base
-
-(require lens
-         racket/contract/base
-         fancy-app)
-
-(module+ test
-  (require rackunit))
-
-(provide
- (contract-out
-  [lens-set-all (->* (any/c any/c) #:rest (listof lens?) any/c)]))
-
-
-(define (lens-set-all target new-view . lenses)
-  (foldl (lens-set _ _ new-view) target lenses))
-
-(module+ test
-  (check-equal? (lens-set-all '(1 2 3 4 5) 'a first-lens third-lens fourth-lens)
-                '(a 2 a a 5)))
+#lang reprovide
+lens/private/view-set/set-all
